5298bc5c2ef39e789582a314ee2b56b961fc9319
Ila <tehpanda32@gmail.com>
2022-11-13 12:55:37 +1100

    master: Add package.json, tsconfig, index.ts, gitignore

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import {projectsRouter} from "./projects/projects.router";
+import * as Process from "process";
+import {tasksRouter} from "./tasks/tasks.router";
+
+require('dotenv').config()
+const app = express();
+export const db = require('./database');
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use('/api/projects', projectsRouter);
+app.use('/api/tasks', tasksRouter);
+
+app.listen(Process.env.NODE_PORT, () => {
+    console.log(`Listening on port ${Process.env.NODE_PORT}`);
+});
